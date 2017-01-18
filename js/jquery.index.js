cba3914fb5724eec8f600498c4c761e6f1ff57f0
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-01-18 18:39:32 +0100

    version 0.12

--- js/jquery.index.js
+++ js/jquery.index.js
@@ -8,7 +8,45 @@
 #notes            : Only working on search engine
 =============================================================*/
 
-function domready{
+function domready() {
+    var allRadios = document.getElementsByName('re');
+    var booRadio;
+    console.log(allRadios.length)
     
+    for(var x = 0; x < allRadios.length; x++){
+        allRadios[x].onclick = function() {
+            if(booRadio == this) {
+                this.checked = false;
+                booRadio = null;
+            } else {
+                booRadio = this;
+            }
+        };
+    }
+    
+    $('button').click(function (){
+        if ($('#search').val() != undefined && $('#search').val() != ''){
+            for (var i=0; i < allRadios.length; i++){
+                if ($('input[name=re]:checked', '#radio-button').val() != undefined){
+                    console.log('toto');
+                    if (i == 0 && $('input[name=re]:checked', '#radio-button').val() == "0")
+                        console.log('artist');
+                        //search_artist($('#search').value);
+                    else if (i == 1 && $('input[name=re]:checked', '#radio-button').val() == "1")
+                        console.log('track');
+                        //search_tracks($('#search').value);
+                    else if (i == 2  && $('input[name=re]:checked', '#radio-button').val() == "2")
+                        console.log('album');
+                        //search_album($('#search').value);
+                }
+            }
+            if ($('input[name=re]:checked', '#radio-button').val() == undefined)
+                console.log("choose one type search 'artist', 'track' or 'album'");
+        }
+        else {
+            console.log('write something in the search bar');
+        }
+    });
 }
 $(document).ready(domready);
+
