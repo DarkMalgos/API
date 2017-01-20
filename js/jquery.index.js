a67f676178826fc2071057e7dfb612e0639ffee1
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-01-20 19:43:12 +0100

    version 0.13

--- js/jquery.index.js
+++ js/jquery.index.js
@@ -9,9 +9,8 @@
 =============================================================*/
 
 function domready() {
-    var allRadios = document.getElementsByName('re');
-    var booRadio;
-    console.log(allRadios.length)
+    var allRadios = document.getElementsByName('re'),
+        booRadio;
     
     for(var x = 0; x < allRadios.length; x++){
         allRadios[x].onclick = function() {
@@ -24,17 +23,19 @@ function domready() {
         };
     }
     
+    
+    
     $('button').click(function (){
         if ($('#search').val() != undefined && $('#search').val() != ''){
             for (var i=0; i < allRadios.length; i++){
                 if ($('input[name=re]:checked', '#radio-button').val() != undefined){
-                    console.log('toto');
                     if (i == 0 && $('input[name=re]:checked', '#radio-button').val() == "0")
                         console.log('artist');
                         //search_artist($('#search').value);
-                    else if (i == 1 && $('input[name=re]:checked', '#radio-button').val() == "1")
-                        console.log('track');
-                        //search_tracks($('#search').value);
+                    else if (i == 1 && $('input[name=re]:checked', '#radio-button').val() == "1") {
+                        var q = $('#search').val();
+                        youtube_track(q);
+                    }
                     else if (i == 2  && $('input[name=re]:checked', '#radio-button').val() == "2")
                         console.log('album');
                         //search_album($('#search').value);
@@ -49,3 +50,25 @@ function domready() {
     });
 }
 $(document).ready(domready);
+
+function youtube_track(q) {
+    var request = gapi.client.youtube.search.list({
+        q: q,
+        part: 'snippet'
+    });
+    request.execute(function(response) {
+        for (var i=0; i < response.result.items.length; i++) {
+            if (response.result.items[i].id.kind == "youtube#video") {
+                var channel = response.result.items[i].snippet.channelTitle;
+                var legit = channel.slice(channel.length-4, channel.length);
+                if (legit == "VEVO")
+                    $('#toto').append('<img src="' + response.result.items[i].snippet.thumbnails.medium.url + '" value="https://www.youtube.com/watch?v=' + response.result.items[i].id.videoId + '"><p>' + response.result.items[i].snippet.title + '</p><p>' + channel + '</p>');
+            }
+
+        }
+    });
+}
+
+function handleAPILoaded() {
+    $('button').attr('disabled', false);
+}
