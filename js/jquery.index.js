5b6d557cf3a4e715f4f9a513e9bc8c814edf0b2e
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-01-26 15:46:49 +0100

    version 0.16

--- js/jquery.index.js
+++ js/jquery.index.js
@@ -12,7 +12,7 @@
 function domready() {
     var allRadios = document.getElementsByName('rad'),
         allLabel = document.getElementsByName('lab'),
-        check = false,
+        check = 'n',
         booRadio = null;
     
     for (var x=0; x < allLabel.length; x++){
@@ -44,7 +44,7 @@ function domready() {
                         var q = $('#search').val();
                         youtube_track(q);
                         soundcloud_track(q);
-                        check = true;
+                        $('footer').css('display', 'inline-block');
                     }
                     else if (i == 2  && $('input[name=rad]:checked', '#choice').val() == "2")
                         console.log('album');
@@ -59,16 +59,45 @@ function domready() {
     
     $('#youtube-result').on('click', 'ul', function() {
         var _url = $(this).attr('value');
-        var _channel = $($(this).find('p')[1]).html();
+        _url = _url.replace("watch?v=", "embed/");
+        var _channel = $($(this).find('p')[0]).html();
         var _artiste;
-        for (var i=0; i < _channel.length; i++) {
-            if (i == 0)
-                _artiste = _channel.charAt(i);
-            else if (i < _channel.length-4)
-                _artiste += _channel.charAt(i);
+        for (var i=0; _channel.charAt(i) != '-'; i++) {
+            if (_channel.charAt(i+1) != '-') {
+                if (i == 0)
+                    _artiste = _channel.charAt(i);
+             
+                else if (_channel.charAt(i) == ' ')
+                    _artiste += '+';
+                else
+                    _artiste += _channel.charAt(i);
+            }
         }
+        $('#result-select').append('<iframe width="672" height="378" src="' + _url + '" frameborder="c0" allowfullscreen></iframe>')
+        var spotify_url = "https://api.spotify.com/v1/search?query=artist:" + _artiste + "&&limit=50&type=album";
+        $.get(spotify_url, function(data){
+            for (var i=0; i < data.albums.items.length; i++){
+                $('#swipper').append('<div class="swiper-slide"><img src=' + data.albums.items[i].images[1].url + ' data-id=' + data.albums.items[i].id + ' ><p>' + data.albums.items[i].name + '</p></div>');
+                
+                if(i === data.albums.items.length-1){
+                    //SWIPPER
+                    var swiper = new Swiper('.swiper-container', {
+                        pagination: '.swiper-pagination',
+                        slidesPerView: 4,
+                        paginationClickable: true,
+                        spaceBetween: 30
+                    });
+                    $(".swiper-slide").click(change_slide);
+                }
+            }
+        });
         $('#home').css('display', 'none');
         $('#search-result').css('display', 'none');
+        $('#result-select').css('display', 'block');
+        $('#disco_bio').css('display', 'block');
+        $('footer').css('background-color', '#e52d27');
+        $('#searchBarf button').css('color', '#e52d27');
+        check = 'y';
     });
     
     $('#soundcloud-result').on('click', 'ul', function() {
@@ -77,9 +106,77 @@ function domready() {
         var _artiste_url = "http://soundcloud.com/" + _artiste;
         $('#home').css('display', 'none');
         $('#search-result').css('display', 'none');
+        check = 's';
+    });
+    
+    $('#bio').hover(function() {
+            if (check == 'y')
+                $('#bio').css('background-color', '#e52d27')
+            else
+                $('#bio').css('background-color', '#ff5500')
+            $('#bio').css('color', 'white')
+            $('#bio').css('border-radius', '5px 5px 0px 0px')
+        });
+    
+    $("#bio").click(function changecolor_bio () {
+
+        $('#disco').css('color', 'black')
+        $('#disco').css('background-color', 'white')
+
+        
+        
+        $('#bio').css('color', 'white')
+        if (check == 'y')
+                $('#bio').css('background-color', '#e52d27')
+            else
+                $('#bio').css('background-color', '#ff5500')
+        $('#bio').css('border-radius', '5px 5px 0px 0px')
+            
+    });
+    
+    $('#disco').hover(function() {
+            if (check == 'y')
+                $('#disco').css('background-color', '#e52d27');
+            else
+                $('#disco').css('background-color', '#ff5500');
+            $('#disco').css('color', 'white')
+            $('#disco').css('border-radius', '5px 5px 0px 0px')
+        });
+    
+    $("#disco").click(function changecolor_disco () {
+        $('#bio').css('color', 'black')
+        $('#bio').css('background-color', 'white')
+        
+        
+        
+        $('#disco').css('color', 'white')
+        if (check == 'y')
+                $('#disco').css('background-color', '#e52d27')
+            else
+                $('#disco').css('background-color', '#ff5500')
+        $('#disco').css('border-radius', '5px 5px 0px 0px')
+            
     });
 }
 $(document).ready(domready);
+
+function change_slide() {
+    var _ids = $(this).find('img').attr('data-id');
+    album = $(this).find('img').attr('src');
+        
+    $("#content").css('display', 'none');
+    console.log(album);
+    $('#content2').append('<img id="albumImg" src="' + album + '">');
+    var _url = "https://api.spotify.com/v1/albums/?ids=" + _ids ;
+    $.get(_url, function(data) {
+        console.log(data); // affiche le résultat au format JSON
+        $('#content2').append('<p id="name-album">' + data.albums[0].name + '-</p><p id="name-artist">' + data.albums[0].artists[0].name + '</p><ul id="category" class="num_track"><li class ="number_tracks">N°</li><li class="name_tracks">Titre</li><li>Artiste</li><li>Album</li><li>Itunes</li></ul>');
+        for (var i=0; i < data.albums[0].tracks.items.length; i++) {
+            $('#content2').append('<ul class="num_track"><li class="number_tracks">' + (i+1) + '</li><li class="name_tracks">' + data.albums[0].tracks.items[i].name + '<li class="play"><img src="../images/play-red.png"</li></ul>');
+        }
+        $('#content2').css('display', 'block');
+    });
+}
 
 function youtube_track(q) {
     var request = gapi.client.youtube.search.list({
@@ -93,7 +190,6 @@ function youtube_track(q) {
                 var channel = response.result.items[i].snippet.channelTitle;
                 var legit = channel.slice(channel.length-4, channel.length);
                 if (legit == "VEVO"){
-                    
                     $('#youtube-result').append('<ul value="https://www.youtube.com/watch?v=' + response.result.items[i].id.videoId + '"><li><img src="' + response.result.items[i].snippet.thumbnails.medium.url + '"></li><li><p>' + response.result.items[i].snippet.title + '</p><p>' + channel + '</p><img src="images/youtube-logo.png"></li>');
                 }
             }
